77880077ce63f32411d38e1c858f0348f9f337ab
Kenneth Chua <kennethchuahwachong@hotmail.com>
2019-07-20 03:19:40 +0800

    Menu with empty fragement and toolbar

--- src/Android/app/src/main/java/kenneth/com/refardenapp/MainActivity.java
+++ src/Android/app/src/main/java/kenneth/com/refardenapp/MainActivity.java
@@ -1,14 +1,14 @@
 package kenneth.com.refardenapp;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.RelativeLayout;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,69 +17,79 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "Main Activity";
-    private TextView textViewTemperature;
-    private TextView textViewSolution;
-    private TextView textViewHumidity;
-    private TextView textViewLight;
-    private TextView textViewWater;
-    private TextView textViewPh;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewTemperature = findViewById(R.id.temperature);
-        textViewSolution = findViewById(R.id.solution);
-        textViewHumidity = findViewById(R.id.humidity);
-        textViewLight = findViewById(R.id.light);
-        textViewWater = findViewById(R.id.water);
-        textViewPh = findViewById(R.id.ph);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Growing Conditions");
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                updateUi(dataSnapshot);
-            }
+        //Navigation Drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
 
     }
 
-    private void updateUi(DataSnapshot dataSnapshot) {
-        for (DataSnapshot ds : dataSnapshot.getChildren()){
-            Log.d(TAG, "ds is:  " + ds.getKey());
-            GrowingConditions growCond = new GrowingConditions();
-            if (ds.getKey().equals("Temperature")) {
-                textViewTemperature.setText(ds.getValue()+ "°C");
-            } else if (ds.getKey().equals("Solution")) {
-                textViewSolution.setText(ds.getValue() + "/1000 ml");
-            } else if (ds.getKey().equals("Humidity")) {
-                textViewHumidity.setText(ds.getValue() + "%");
-            } else if (ds.getKey().equals("Light")) {
-                textViewLight.setText(ds.getValue() + "lm");
-            } else if (ds.getKey().equals("Water")) {
-                textViewWater.setText(ds.getValue() + "/1000 ml");
-            } else if (ds.getKey().equals("Ph")) {
-                textViewPh.setText(ds.getValue() + "%");
-            }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
 
+            case R.id.nav_my_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+
+            case R.id.nav_trade_market:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TradeFragment()).commit();
+                break;
+
+            case R.id.nav_forum:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ForumFragment()).commit();
+                break;
+
+            case R.id.nav_notifications:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NotificationsFragment()).commit();
+                break;
+
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                break;
+
+            case R.id.nav_help:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HelpFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
         }
     }
+
+
 }
